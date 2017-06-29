ac338c4e2b9bc02bbc169da18733df928b621140
t0tem <vadimkul@gmail.com>
2017-06-29 10:57:43 +0200

    initial commit

---
+++ MyApp1/ui.R
@@ -0,0 +1,12 @@
+library(shiny)
+shinyUI(fluidPage(
+    titlePanel(title = ""),
+    sidebarLayout(
+        sidebarPanel(
+            
+        ),
+        mainPanel(
+            
+        )
+    )
+))
