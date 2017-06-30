c802809248440351333de04ef4e9aa9c554aa965
t0tem <vadimkul@gmail.com>
2017-06-30 16:37:24 +0200

    first draft version. working :)

--- MyApp1/server.R
+++ MyApp1/server.R
@@ -1,4 +1,18 @@
 library(shiny)
+
 shinyServer(function(input, output) {
+    data <- ggplot2::diamonds
+    fit <- lm(price ~ carat + cut + color + clarity, data = data)
+    
+    pred <- reactive({
+        newData <- data.frame(carat = input$carat,
+                              cut = input$cut,
+                              color = input$color,
+                              clarity = input$clarity)
+        
+        predict(fit, newdata = newData)
+    })
+    
+    output$result <- renderText({pred()})
     
 })
--- MyApp1/ui.R
+++ MyApp1/ui.R
@@ -1,20 +1,43 @@
 library(shiny)
+
 shinyUI(fluidPage(
+    
     titlePanel(title = "Diamond price prediction tool", windowTitle = "Diamond price prediction tool"),
     hr(),
-    # fluidRow(column(4, h1("foo foo foo foo foofofo")), column(8, h1("value
-    # value value valuevalue"))), 
-    
     sidebarLayout(
-        sidebarPanel(title = "text",
-            h3("Choose the parameters of your diamond below")
-            
-           
+        
+        sidebarPanel(
+            tabsetPanel(
+                tabPanel(
+                    title = "Manual input",
+                    h4(em("Choose the parameters of your diamond below")),
+                    hr(),
+                    sliderInput("carat", label = "Choose carat", min = 0.2, 
+                                max = 20, value = 0.2, step = 0.1),
+                    radioButtons("cut", label = "Choose cut",
+                                 choices = list("Fair", "Good", "Very Good",
+                                                "Premium", "Ideal"), 
+                                 selected = "Good", inline = TRUE),
+                    selectInput("color", label = "Choose color",
+                                choices = list("J (worst)" = "J",
+                                               "I","H","G","F","E",
+                                               "D (best)" = "D"), 
+                                selected = "D"),
+                    selectInput("clarity", label = "Choose clarity",
+                                choices = list("I1 (worst)" = "I1", "SI1", 
+                                               "SI2", "VS1", "VS2", "VVS1",
+                                               "VVS2", "IF (best)" = "IF"), 
+                                selected = "IF")
+                ),
+                tabPanel(
+                    title = "Random simulation"
+                )
+            )
         ),
-       
         mainPanel(
-            h3("Predicted price of your diamond is")
-            
+            br(),
+            h4("Predicted price of your diamond is"),
+            p(textOutput("result"))
         )
     )
 ))
