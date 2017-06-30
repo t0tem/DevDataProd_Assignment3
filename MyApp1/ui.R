1a8fd68fb2f3abdd13abc10441ab6a746aa599e5
t0tem <vadimkul@gmail.com>
2017-06-30 11:08:56 +0200

    update

--- MyApp1/ui.R
+++ MyApp1/ui.R
@@ -1,11 +1,19 @@
 library(shiny)
 shinyUI(fluidPage(
-    titlePanel(title = ""),
+    titlePanel(title = "Diamond price prediction tool", windowTitle = "Diamond price prediction tool"),
+    hr(),
+    # fluidRow(column(4, h1("foo foo foo foo foofofo")), column(8, h1("value
+    # value value valuevalue"))), 
+    
     sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(title = "text",
+            h3("Choose the parameters of your diamond below")
             
+           
         ),
+       
         mainPanel(
+            h3("Predicted price of your diamond is")
             
         )
     )
